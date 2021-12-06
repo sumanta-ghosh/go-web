0f377190f62f123edc3d3c37804730c0ed5bc9d7
Sumanta <sumanta.ghosh.mit@gmail.com>
2021-12-07 01:06:33 +0530

    Connect with mysql

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module go-web
 go 1.13
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 )
--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
+type Env struct {
+	dbHost, dbUser, dbPass, dbName string
+}
+
 func main() {
 	router := mux.NewRouter()
-
+	dbCon := Env{
+		dbHost: "192.168.0.100",
+		dbUser: "db_user",
+		dbPass: "Proj123",
+		dbName: "go_web_api",
+	}
 	router.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		dbCon.connectDb()
 		fmt.Fprintf(rw, "Welcome to Golang home page...")
 	})
 
@@ -27,5 +39,13 @@ func main() {
 
 	if err := http.ListenAndServe(":8085", router); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func (env Env) connectDb() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s:3306)/%s?parseTime=true", env.dbUser, env.dbPass, env.dbHost, env.dbName))
+
+	if db == nil && err != nil {
+		fmt.Printf(" Connection error ")
 	}
 }
